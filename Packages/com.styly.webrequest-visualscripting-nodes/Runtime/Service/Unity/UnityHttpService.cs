e402450dfdce26f1d718456bba33452ed5d45d97
Masahiro Yamaguchi <from2001@hotmail.com>
2024-01-07 20:15:14 +0900

    Updated obsolete and deprecated usages.

--- Packages/com.styly.webrequest-visualscripting-nodes/Runtime/Service/Unity/UnityHttpService.cs
+++ Packages/com.styly.webrequest-visualscripting-nodes/Runtime/Service/Unity/UnityHttpService.cs
@@ -6,35 +6,46 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 
-namespace STYLY.Http.Service.Unity {
-    public class UnityHttpService : IHttpService {
-        public IHttpRequest Get(string uri) {
+namespace STYLY.Http.Service.Unity
+{
+    public class UnityHttpService : IHttpService
+    {
+        public IHttpRequest Get(string uri)
+        {
             return new UnityHttpRequest(UnityWebRequest.Get(uri));
         }
 
-        public IHttpRequest GetTexture(string uri) {
+        public IHttpRequest GetTexture(string uri)
+        {
             return new UnityHttpRequest(UnityWebRequestTexture.GetTexture(uri));
         }
 
-        public IHttpRequest Post(string uri, string postData) {
+        public IHttpRequest Post(string uri, string postData)
+        {
             return new UnityHttpRequest(UnityWebRequest.PostWwwForm(uri, postData));
         }
 
-        public IHttpRequest Post(string uri, WWWForm formData) {
+        public IHttpRequest Post(string uri, WWWForm formData)
+        {
             return new UnityHttpRequest(UnityWebRequest.Post(uri, formData));
         }
 
-        public IHttpRequest Post(string uri, Dictionary<string, string> formData) {
+        public IHttpRequest Post(string uri, Dictionary<string, string> formData)
+        {
             return new UnityHttpRequest(UnityWebRequest.Post(uri, formData));
         }
 
-        public IHttpRequest Post(string uri, List<IMultipartFormSection> multipartForm) {
+        public IHttpRequest Post(string uri, List<IMultipartFormSection> multipartForm)
+        {
             return new UnityHttpRequest(UnityWebRequest.Post(uri, multipartForm));
         }
 
-        public IHttpRequest Post(string uri, byte[] bytes, string contentType) {
-            var unityWebRequest = new UnityWebRequest(uri, UnityWebRequest.kHttpVerbPOST) {
-                uploadHandler = new UploadHandlerRaw(bytes) {
+        public IHttpRequest Post(string uri, byte[] bytes, string contentType)
+        {
+            var unityWebRequest = new UnityWebRequest(uri, UnityWebRequest.kHttpVerbPOST)
+            {
+                uploadHandler = new UploadHandlerRaw(bytes)
+                {
                     contentType = contentType
                 },
                 downloadHandler = new DownloadHandlerBuffer()
@@ -42,32 +53,39 @@ namespace STYLY.Http.Service.Unity {
             return new UnityHttpRequest(unityWebRequest);
         }
 
-        public IHttpRequest PostJson(string uri, string json) {
+        public IHttpRequest PostJson(string uri, string json)
+        {
             return Post(uri, Encoding.UTF8.GetBytes(json), "application/json");
         }
 
-        public IHttpRequest PostJson<T>(string uri, T payload) where T : class {
+        public IHttpRequest PostJson<T>(string uri, T payload) where T : class
+        {
             return PostJson(uri, JsonUtility.ToJson(payload));
         }
 
-        public IHttpRequest Put(string uri, byte[] bodyData) {
+        public IHttpRequest Put(string uri, byte[] bodyData)
+        {
             return new UnityHttpRequest(UnityWebRequest.Put(uri, bodyData));
         }
 
-        public IHttpRequest Put(string uri, string bodyData) {
+        public IHttpRequest Put(string uri, string bodyData)
+        {
             return new UnityHttpRequest(UnityWebRequest.Put(uri, bodyData));
         }
 
-        public IHttpRequest Delete(string uri) {
+        public IHttpRequest Delete(string uri)
+        {
             return new UnityHttpRequest(UnityWebRequest.Delete(uri));
         }
 
-        public IHttpRequest Head(string uri) {
+        public IHttpRequest Head(string uri)
+        {
             return new UnityHttpRequest(UnityWebRequest.Head(uri));
         }
 
         public IEnumerator Send(IHttpRequest request, Action<HttpResponse> onSuccess = null,
-            Action<HttpResponse> onError = null, Action<HttpResponse> onNetworkError = null) {
+            Action<HttpResponse> onError = null, Action<HttpResponse> onNetworkError = null)
+        {
             var unityHttpRequest = (UnityHttpRequest)request;
             var unityWebRequest = unityHttpRequest.UnityWebRequest;
 
@@ -75,37 +93,46 @@ namespace STYLY.Http.Service.Unity {
 
             var response = CreateResponse(unityWebRequest);
 
-            if (unityWebRequest.isNetworkError) {
+            if (unityWebRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
                 onNetworkError?.Invoke(response);
             }
-            else if (unityWebRequest.isHttpError) {
+            else if (unityWebRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
                 onError?.Invoke(response);
             }
-            else {
+            else
+            {
                 onSuccess?.Invoke(response);
             }
         }
 
-        public void Abort(IHttpRequest request) {
+        public void Abort(IHttpRequest request)
+        {
             var unityHttpRequest = request as UnityHttpRequest;
-            if (unityHttpRequest?.UnityWebRequest != null && !unityHttpRequest.UnityWebRequest.isDone) {
+            if (unityHttpRequest?.UnityWebRequest != null && !unityHttpRequest.UnityWebRequest.isDone)
+            {
                 unityHttpRequest.UnityWebRequest.Abort();
             }
         }
 
-        private static HttpResponse CreateResponse(UnityWebRequest unityWebRequest) {
-            return new HttpResponse {
+        private static HttpResponse CreateResponse(UnityWebRequest unityWebRequest)
+        {
+            return new HttpResponse
+            {
                 Url = unityWebRequest.url,
                 Bytes = unityWebRequest.downloadHandler?.data,
                 Text = unityWebRequest.downloadHandler?.text,
-                IsSuccessful = !unityWebRequest.isHttpError && !unityWebRequest.isNetworkError,
-                IsHttpError = unityWebRequest.isHttpError,
-                IsNetworkError = unityWebRequest.isNetworkError,
+                IsSuccessful = unityWebRequest.result != UnityWebRequest.Result.ProtocolError
+                            && unityWebRequest.result != UnityWebRequest.Result.ConnectionError,
+                IsHttpError = unityWebRequest.result == UnityWebRequest.Result.ProtocolError,
+                IsNetworkError = unityWebRequest.result == UnityWebRequest.Result.ConnectionError,
                 Error = unityWebRequest.error,
                 StatusCode = unityWebRequest.responseCode,
                 ResponseHeaders = unityWebRequest.GetResponseHeaders(),
                 Texture = (unityWebRequest.downloadHandler as DownloadHandlerTexture)?.texture
             };
         }
+
     }
 }
