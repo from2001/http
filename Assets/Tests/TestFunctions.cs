a8a55a03177a8e3ff0f937614e1e14141dce3b7a
Masahiro Yamaguchi <from2001@hotmail.com>
2024-04-12 18:34:42 +0900

    Added test code

---
+++ Assets/Tests/TestFunctions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using STYLY.Http;
+using STYLY.Http.Service;
+using NUnit.Framework;
+
+public class TestFunctions : MonoBehaviour
+{
+    static public void LogAndAssert(HttpResponse ResultResponse,bool?isMethodSucceeded, bool shouldSucceed)
+    {
+        if (((bool)isMethodSucceeded && shouldSucceed) || (!(bool)isMethodSucceeded && !shouldSucceed))
+        {
+            Debug.Log("StatusCode: " + ResultResponse.StatusCode);
+            Debug.Log("Error Message: " + ResultResponse.Error);
+            Debug.Log("Text: " + ResultResponse.Text);
+            Assert.Pass("Passed");
+        }
+        else
+        {
+            Debug.Log("StatusCode: " + ResultResponse.StatusCode);
+            Debug.Log("Error Message: " + ResultResponse.Error);
+            Debug.Log("Text" + ResultResponse.Text);
+            Assert.Fail("Failed");
+        }
+    }
+}
